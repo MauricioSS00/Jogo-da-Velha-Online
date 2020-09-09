e0c53df1e06cec4ea31626d09f14c3e7a8e73c1c
Maurício da Silva <maurcioss83@gmail.com>
2020-09-08 22:55:16 -0300

    Concluíndo Jogabilidade

--- src/app/dados/dados.page.ts
+++ src/app/dados/dados.page.ts
@@ -11,6 +11,8 @@ export class DadosPage implements OnInit {
   nick1: string = '';
   nick2: string = '';
   online: boolean = false;
+  icone1: string = 'x';
+  icone2: string = 'o';
 
   constructor(
     private router: Router
@@ -25,9 +27,19 @@ export class DadosPage implements OnInit {
         partida: {
           nick1: this.nick1,
           nick2: this.nick2,
+          icone1: this.icone1,
+          icone2: this.icone2,
           online: this.online
         }
       }
     });
+  }
+
+  selecionouIcone(jogador: number) {
+    if (jogador == 1) {
+      this.icone2 = this.icone1 == 'x' ? 'o' : 'x';
+    } else {
+      this.icone1 = this.icone2 == 'x' ? 'o' : 'x';
+    }
   }
 }
--- src/app/partida/partida.page.ts
+++ src/app/partida/partida.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-partida',
@@ -10,28 +11,88 @@ export class PartidaPage implements OnInit {
 
   partida: any;
   jogadorAtual: string;
+  iconeAtual: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private alertController: AlertController
   ) { }
 
   ngOnInit() {
     if (this.router.getCurrentNavigation().extras.state) {
       this.partida = this.router.getCurrentNavigation().extras.state.partida;
     } else {
-      this.partida = { nick1: "Maurício", nick2: "Ketlin", online: true }
+      this.partida = { nick1: 'Maurício', nick2: 'Ketlin', icone1: 'x', icone2: 'o', online: true };
     }
     this.jogadorAtual = this.partida.nick1;
-    this.partida.label1 = 'x';
-    this.partida.label2 = 'o';
-    this.partida.label3 = 'x';
-    this.partida.label4 = 'o';
-    this.partida.label5 = 'x';
-    this.partida.label6 = 'o';
-    this.partida.label7 = 'x';
-    this.partida.label8 = 'o';
-    this.partida.label9 = 'x';
-    console.log(this.partida);
+    this.iconeAtual = this.partida.icone1;
+    this.partida.label = [];
   }
 
+  async jogar(label: number) {
+    if (!this.partida.label[label]) {
+      this.partida.label[label] = this.iconeAtual;
+      const resultado = await this.checarVitoria();
+      if (!resultado) {
+        const velha = await this.checarVelha();
+        if (!velha) {
+          this.alternarJogador();
+        } else {
+          this.exibirAlerta('Não foi dessa Vez!', `Ninguém ganhou, que pena, mas vamos para a proxima!`);
+        }
+      } else {
+        this.exibirAlerta('Vitória!!!!', `Parabéns ${this.jogadorAtual} pela vitória!`);
+      }
+    }
+  }
+
+  alternarJogador() {
+    if (this.jogadorAtual === this.partida.nick1) {
+      this.jogadorAtual = this.partida.nick2;
+      this.iconeAtual = this.partida.icone2;
+    } else {
+      this.jogadorAtual = this.partida.nick1;
+      this.iconeAtual = this.partida.icone1;
+    }
+  }
+
+  async checarVitoria() {
+    if (this.partida.label[1] && this.partida.label[1] === this.partida.label[2] && this.partida.label[1] === this.partida.label[3]) {
+      return true;
+    } else if (this.partida.label[4] && this.partida.label[4] === this.partida.label[5] && this.partida.label[4] === this.partida.label[6]) {
+      return true;
+    } else if (this.partida.label[7] && this.partida.label[7] === this.partida.label[8] && this.partida.label[7] === this.partida.label[9]) {
+      return true;
+    } else if (this.partida.label[1] && this.partida.label[1] === this.partida.label[4] && this.partida.label[1] === this.partida.label[7]) {
+      return true;
+    } else if (this.partida.label[2] && this.partida.label[2] === this.partida.label[5] && this.partida.label[2] === this.partida.label[8]) {
+      return true;
+    } else if (this.partida.label[3] && this.partida.label[3] === this.partida.label[6] && this.partida.label[3] === this.partida.label[9]) {
+      return true;
+    } else if (this.partida.label[1] && this.partida.label[1] === this.partida.label[5] && this.partida.label[1] === this.partida.label[9]) {
+      return true;
+    } else if (this.partida.label[3] && this.partida.label[3] === this.partida.label[5] && this.partida.label[3] === this.partida.label[7]) {
+      return true;
+    }
+    return false;
+  }
+
+  async checarVelha() {
+    if (this.partida.label[1] && this.partida.label[2] && this.partida.label[3] &&
+      this.partida.label[4] && this.partida.label[5] && this.partida.label[6] &&
+      this.partida.label[7] && this.partida.label[8] && this.partida.label[9]) {
+      return true;
+    }
+    return false;
+  }
+
+  async exibirAlerta(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
